a7b31e7641c872b7a4abde47f5b25970ef90019c
David Castellanos Zayas <david383812@gmail.com>
2025-03-12 23:15:17 -0400

    cambiando el firebasecongif

--- src/service/config.tsx
+++ src/service/config.tsx
@@ -7,12 +7,12 @@ export const SOCKET_URL = process.env.EXPO_PUBLIC_BASE_URLWS
 
 
 export const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY || "",
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN || "",
-  databaseURL: process.env.FIREBASE_DATABASE_URL || "",
-  projectId: process.env.FIREBASE_PROJECT_ID || "",
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET || "",
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || "",
-  appId: process.env.FIREBASE_APP_ID || "",
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID || ""
+  apiKey: "AIzaSyBVWYHKgp_9b95zaFtVwI1ekS9XirOcBV0",
+  authDomain: "rapidoapp-4a547.firebaseapp.com",
+  databaseURL: "https://rapidoapp-4a547-default-rtdb.firebaseio.com",
+  projectId: "rapidoapp-4a547",
+  storageBucket: "rapidoapp-4a547.firebasestorage.app",
+  messagingSenderId: "547406702474",
+  appId: "1:547406702474:web:d2a0f1ed1c6b2b3dca4a73",
+  measurementId: "G-MWJPFJMSLT"
 };
