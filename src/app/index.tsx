4f46ec2fbf5c02cdbe8c3449a01e6e1b6142519a
David Castellanos Zayas <david383812@gmail.com>
2025-03-13 18:47:17 -0400

    muy mal

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import { View, Image, Alert,PermissionsAndroid  } from 'react-native'
-=======
 import { View, Image, Alert,  } from 'react-native'
->>>>>>> 4f16ab7 (probando con firebase)
 import React, { useEffect, useState } from 'react'
 import { commonStyles } from '@/styles/commonStyles'
 import { splashStyles } from '@/styles/splashStyles'
@@ -21,14 +17,7 @@ interface DecodedToken {
 
 
 const Main = () => {
-<<<<<<< HEAD
-  messaging().setBackgroundMessageHandler(async remoteMessage => {
-    console.log('Message handled in the background!', remoteMessage);
-  });
-
-=======
   
->>>>>>> 4f16ab7 (probando con firebase)
   const [loaded] = useFonts({
     Bold: require("../assets/fonts/NotoSans-Bold.ttf"),
     Regular: require("../assets/fonts/NotoSans-Regular.ttf"),
