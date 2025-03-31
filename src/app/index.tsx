9ae3c29ea91e35c109cb32353ec740c741ef515f
David Castellanos Zayas <david383812@gmail.com>
2025-03-31 16:20:02 -0400

    Agregar verificación de versión y manejo de actualizaciones en la pantalla principal

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,19 @@
-import { View, Image, Alert } from 'react-native'
+import { View, Image, Alert, Modal, Text, Button, Linking, StyleSheet, Pressable, ActivityIndicator } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { commonStyles } from '@/styles/commonStyles'
 import { splashStyles } from '@/styles/splashStyles'
 import CustomText from '@/components/shared/CustomText'
-import {useFonts} from "expo-font"
+import { useFonts } from "expo-font"
 import { resetAndNavigate } from '@/utils/Helpers'
-import {jwtDecode} from 'jwt-decode'
+import { jwtDecode } from 'jwt-decode'
 import { tokenStorage } from '@/store/storage'
 import { refresh_tokens } from '@/service/apiInterceptors'
 import { useUserStore } from '@/store/userStore'
+import * as Application from 'expo-application';
+import { type Version } from '@/utils/types'
+import { MaterialIcons } from '@expo/vector-icons'
+import { authStyles } from '@/styles/authStyles'
+
 
 interface DecodedToken {
   exp: number
@@ -23,26 +28,60 @@ const Main = () => {
     SemiBold: require("../assets/fonts/NotoSans-SemiBold.ttf"),
   })
 
-  const {user} = useUserStore()
+  const { user } = useUserStore()
 
   const [hasNavigated, setHasNavigated] = useState(false)
 
-  const tokenCheck = async() => {
+  const [updateRequired, setUpdateRequired] = useState(false);
+  const [downloadUrl, setDownloadUrl] = useState('');
+  const [errorMessage, setError] = useState(false);
+  const [loading, setLoading] = useState(true); // Nuevo estado
+
+  const checkVersion = async () => {
+
+    try {
+      // throw new Error("error")
+      const response = await fetch('https://server-react-native-app.onrender.com/version');
+      const data: Version = await response.json();
+      const latestVersion = data.version.version;
+      const currentVersion = Application.nativeApplicationVersion;
+      console.log(currentVersion);
+
+      const isUpdateRequired = (latestVersion !== currentVersion);
+      if (isUpdateRequired) {
+        setDownloadUrl("http://www.apklis.cu");
+        setUpdateRequired(true);
+      }
+      return isUpdateRequired;
+    } catch (error) {
+      // Alert.alert("Error de Comprobación", "No se pudo acceder al servidor para validar la versión de su dispositivo")
+      console.error('Error al comprobar la versión:', error);
+      setError(true)// O bien, considerar cómo manejar el error
+      return true
+    }
+  };
+
+  const retryCheckVersion = () => {
+    setError(false); // Reinicia el error
+    checkVersion(); // Vuelve a verificar la versión
+  };
+
+  const tokenCheck = async () => {
     const access_token = tokenStorage.getString("access_token") as string;
     const refresh_token = tokenStorage.getString("refresh_token") as string;
 
-    if(access_token){
+    if (access_token) {
       const decodedAccessToken = jwtDecode<DecodedToken>(access_token);
       const decodedRefreshToken = jwtDecode<DecodedToken>(refresh_token);
 
       const currentTime = Date.now() / 1000;
 
-      if(decodedRefreshToken?.exp < currentTime){
+      if (decodedRefreshToken?.exp < currentTime) {
         resetAndNavigate("/role")
         Alert.alert("Su sesión ha expirado, por favor vuelva a iniciar")
       }
 
-      if(decodedAccessToken?.exp < currentTime){
+      if (decodedAccessToken?.exp < currentTime) {
         try {
           refresh_tokens()
         } catch (err) {
@@ -51,40 +90,132 @@ const Main = () => {
         }
       }
 
-      if(user){
+      if (user) {
         resetAndNavigate("/customer/home")
-      }else{
+      } else {
         resetAndNavigate("/captain/home")
       }
-      
+
       return
     }
 
     resetAndNavigate('/role')
   }
 
+
   useEffect(() => {
-    if(loaded && !hasNavigated){
-      const timeoutId = setTimeout(()=> {
-        tokenCheck()
-        setHasNavigated(true)
+    if (loaded && !hasNavigated) {
+      const timeoutId = setTimeout(async () => {
+
+        const isUpdateRequired = await checkVersion();
+        if (!isUpdateRequired && !errorMessage) {
+          await tokenCheck();
+          setHasNavigated(true);
+        }
       }, 1000);
-      return () => clearTimeout(timeoutId)
+      return () => clearTimeout(timeoutId);
     }
-  }, [loaded, hasNavigated])
+  }, [loaded, hasNavigated, errorMessage]);
+
+
+
 
 
   return (
-    <View style={commonStyles.container}>
-      <Image
-      source={require("@/assets/images/logo_t.png")}
-        style={splashStyles.img}
-      />
-      <CustomText variant='h8' fontFamily='Light' style={splashStyles.text}>
-        Desarrollado con ❤ en 🇨🇺
-      </CustomText>
-    </View>
-  )
+    <>
+      {updateRequired && (
+        <Modal visible={true} transparent={true}>
+          <View style={styles.modalContainer}>
+            <View style={styles.dialog}>
+              <Text style={styles.dialogText}>
+                "Actualiza la app para disfrutar de nuevas funciones y seguridad."
+              </Text>
+              <View style={{ flexDirection: "row", justifyContent: "space-around", width: "100%", alignItems: "center" }}>
+                <Pressable onPress={() => Linking.openURL(downloadUrl)}>
+                  <Text style={{ color: "#ffc920", fontWeight: "bold", fontSize: 17 }}>Actualizar</Text>
+                </Pressable>
+                <Pressable style={authStyles.flexRowGap} onPress={() => Linking.openURL('https://t.me/rapidoappcuba')}  >
+                  <MaterialIcons name="telegram" size={24} style={{ color: "#24A1DE" }} />
+                  <Text style={{ color: "#ffc920", fontWeight: "bold", fontSize: 17 }}>Soporte</Text>
+                </Pressable>
+              </View>
+
+            </View>
+          </View>
+        </Modal>
+      )}
+
+      {
+        errorMessage && (
+          <Modal visible={true} transparent={true}>
+            <View style={styles.modalContainer}>
+              <View style={styles.dialog}>
+                <Text style={styles.dialogText}>
+                  No hay conexión
+                </Text>
+                <Text style={{ marginBottom: 5, textAlign: "center" }}>
+                  Al pulsar en Reintentar voleremos a intentar conectarte
+                </Text>
+                <Pressable onPress={() => retryCheckVersion()}>
+                  {({ pressed }) => (
+                    <Text style={[
+                      { color: "#ffc920", fontWeight: "bold", fontSize: 17 },
+                      pressed && { color: "black" } // Color más oscuro al presionar
+                    ]}>
+                      Reintentar
+                    </Text>
+                  )}
+                </Pressable>
+              </View>
+            </View>
+          </Modal>
+        )
+      }
+      <View style={commonStyles.container}>
+        <ActivityIndicator size="large" color={"#ffc920"} />
+        <Image
+          source={require("@/assets/images/logo_t.png")}
+          style={splashStyles.img}
+        />
+        <CustomText variant="h8" fontFamily="Light" style={splashStyles.text}>
+          Desarrollado con ❤ en 🇨🇺
+        </CustomText>
+      </View>
+    </>
+  );
+
 }
 
 export default Main
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  loadingContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  modalContainer: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  dialog: {
+    width: '80%',
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 20,
+    alignItems: 'center',
+    height: 150,
+  },
+  dialogText: {
+    fontSize: 17,
+    marginBottom: 25,
+    textAlign: 'center'
+  }
+});
