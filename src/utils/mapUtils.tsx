55bd680ef289a5b9e1f38029366c2a9ad8862645
David Castellanos Zayas <david383812@gmail.com>
2025-03-31 16:18:29 -0400

    Actualizar la tarifa por kilómetro en la función de cálculo de tarifas y redondear el resultado al múltiplo de 10

--- src/utils/mapUtils.tsx
+++ src/utils/mapUtils.tsx
@@ -98,7 +98,7 @@ export const calculateDistance = ({lat1, lon1, lat2, lon2}: calculateDistanceTyp
 
 export const calculateFare = (distance: number) => {
     const rateStructure = {
-        bike: { baseFare: 100, perKmRate: 50, minimumFare: 50 },
+        bike: { baseFare: 100, perKmRate: 100, minimumFare: 50 },
         auto: { baseFare: 200, perKmRate: 100, minimumFare: 100 },
         cabEconomy: { baseFare: 200, perKmRate: 100, minimumFare: 400 },
         cabPremium: { baseFare: 200, perKmRate: 200, minimumFare: 500 },
@@ -106,7 +106,8 @@ export const calculateFare = (distance: number) => {
 
     const fareCalculation = (baseFare: number, perKmRate: number, minimumFare: number) => {
         const calculatedFare = baseFare + (distance * perKmRate);
-        return Math.max(calculatedFare, minimumFare);
+        const maxFare = Math.max(calculatedFare, minimumFare);
+        return Math.floor(maxFare / 10) * 10; // Redondeo hacia abajo al múltiplo de 10
     };
 
     return {
