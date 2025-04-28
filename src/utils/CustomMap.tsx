273c845918b0d6464edbfde49eba82a7983189da
David Castellanos Zayas <david383812@gmail.com>
2025-04-28 19:29:46 -0400

    feat: agregar lógica para calcular la región inicial basada en la provincia del usuario

--- src/utils/CustomMap.tsx
+++ src/utils/CustomMap.tsx
@@ -1,3 +1,5 @@
+import { useUserStore } from "@/store/userStore"
+
 export const customMapStyle = [
   {
     "featureType": "administrative.land_parcel",
@@ -53,10 +55,48 @@ export const customMapStyle = [
   }
 ]
 
+const ListOfRegion = [
+   {
+    name: "Pinar del Rio",
+    latitude: 22.4124409,
+    longitude: -83.7339696,
+    latitudeDelta: 0.5,
+    longitudeDelta: 0.5,
+  },
+   {
+    name: "Artemisa",
+    latitude: 22.7543853,
+    longitude: -83.024653,
+    latitudeDelta: 0.5,
+    longitudeDelta: 0.5,
+  },
+  {
+    name: "Havana",
+    latitude: 23.0591519,
+    longitude: -82.4441138,
+    latitudeDelta: 0.5,
+    longitudeDelta: 0.5,
+  },
+  {
+    name: "Mayabeque",
+    latitude: 22.8251554,
+    longitude: -82.3598366,
+    latitudeDelta: 0.5,
+    longitudeDelta: 0.5,
+  }
+]
+
+export const calcInitialRegion = () => {
+  const { user } = useUserStore()
+  const finallCity = ListOfRegion.find(e => e.name == user.user_metadata.province)
+  return user.user_metadata.province
+
+}
+
 export const tunasIntialRegion = {
   latitude: 21.8955318,
   longitude: -79.4665393,
-  latitudeDelta: 0.5,
-  longitudeDelta: 0.5,
+  latitudeDelta: 0.01,
+  longitudeDelta: 0.01,
 }
 // 20.962636656524953, -76.96067947257785
